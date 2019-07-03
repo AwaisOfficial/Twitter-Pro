1c408d24c3d68284cc3e0b2e076d769dd7659cf6
AwaisOfficial <im.awais.official@gmail.com>
2019-07-03 20:41:11 +0500

    deploying to heroku.

--- server/app.js
+++ server/app.js
@@ -19,8 +19,8 @@ class App {
     }
     config() {
         var distDir = __dirname + "/dist/";
-        this.app.use(express_1.static(distDir));
-        
+        this.app.use(express_1.default.static(distDir));
+
         this.app.use(cors());
         this.app.use(body_parser_1.default.json());
         this.app.use(body_parser_1.default.urlencoded({ extended: false }));
