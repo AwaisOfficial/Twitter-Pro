d9a962e60813328c235d18e476d645c183aeaa61
AwaisOfficial <im.awais.official@gmail.com>
2019-07-04 10:40:57 +0500

    deploying to heroku

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./server/app"));
-const http = __importStar(require("http"));
-const config_1 = require("./server/config/config");
+var express = require('express');
+var path =  require('path');
 
-// const httpOptions = {
-//     key : fs.readFileSync(path.resolve("../certificates/key.pem")),
-//     cert: fs.readFileSync(path.resolve("../certificates/cert.pem"))
-// }
-// https.createServer({}, app).listen(PORT , () => {
-//     console.log('Express server listening on port '+ PORT);
-// });
-http.createServer(app_1.default).listen(process.env.PORT || config_1.PORT, () => {
-    console.log('Express server listening on port ' + config_1.PORT);
+var app = express();
+
+app.use(express.static(__dirname + '/dist/TwitterPro/'));
+
+app.listen(process.env.PORT || 3000 ,() => console.log("Express Server listening."));
+
+app.get('/*' , (req , res) => {
+    res.sendFile(path.join(__dirname + '/dist/TwitterPro/index.html'));
 });
