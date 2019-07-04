4e13ec617af9f1b12566b24fab8435367e8980dc
AwaisOfficial <im.awais.official@gmail.com>
2019-07-04 11:26:19 +0500

    deploying to heroku

--- client/app/constants/constants.ts
+++ client/app/constants/constants.ts
@@ -1,5 +1,5 @@
 export const APP_NAME   = 'Twitter Pro';
-export const SERVER_URL = 'http://localhost:3000/';//
+export const SERVER_URL = 'https://floating-mountain-61938.herokuapp.com/api/';// http://localhost:4200/api/
 //export const SERVER_URL = 'http://getwebsite.com.pk/dev/server/';
 export const JWTOKEN = 'twitter_pro_token';
 export const USER_INFO = 'twitter_pro_user';
