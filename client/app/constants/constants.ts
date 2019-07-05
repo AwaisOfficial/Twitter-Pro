17faf92e25f5dcaf101a74acbaa30e9475c6f0ce
AwaisOfficial <im.awais.official@gmail.com>
2019-07-05 14:40:47 +0500

    Authentication work is done.

--- client/app/constants/constants.ts
+++ client/app/constants/constants.ts
@@ -1,5 +1,5 @@
 export const APP_NAME   = 'Twitter Pro';
-export const SERVER_URL = 'https://floating-mountain-61938.herokuapp.com/api/';
+export const SERVER_URL = 'https://twitterpro.herokuapp.com/api/';
 //export const SERVER_URL = 'http://localhost:3000/api/';
 export const JWTOKEN = 'twitter_pro_token';
 export const USER_INFO = 'twitter_pro_user';
