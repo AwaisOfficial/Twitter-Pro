2e125856ad8a0f755f16514d1b6bfcefd835422f
AwaisOfficial <im.awais.official@gmail.com>
2019-07-05 12:18:05 +0500

    Authentication work is done.

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const multer = require('multer');
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './src/uploads');
+        cb(null, './server/uploads');
     },
     filename: (req, file, cb) => {
         const filename = new Date().getTime() + '_' + file.originalname;
