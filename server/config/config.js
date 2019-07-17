36616228e221d2de05925ddf5286bde5cc82db5d
AwaisOfficial <im.awais.official@gmail.com>
2019-07-17 17:14:50 +0500

    sign-in with twitter is done.

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CLIENT_URL = 'http://twitterpro.herokuapp.com';
+exports.CLIENT_URL = 'https://twitterpro.herokuapp.com';
 exports.DATABASE_URL = process.env.DATABASE || 'mongodb+srv://Awais:awais31@twitterclone-cfqlw.mongodb.net/TwitterPro?retryWrites=true&w=majority';
 exports.JWTSECRET = '$QET_UWN';
 exports.PORT = 3000;
