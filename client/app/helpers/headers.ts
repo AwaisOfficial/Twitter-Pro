872c7bab4194e470b80fc5a102a59ba9aab636b9
AwaisOfficial <im.awais.official@gmail.com>
2019-07-25 16:09:53 +0500

    twitter issue fixed.

--- client/app/helpers/headers.ts
+++ client/app/helpers/headers.ts
@@ -1,17 +1,19 @@
 import { HttpHeaders } from '@angular/common/http';
+import { JWTOKEN } from '../constants/constants';
 
 export class Http_Headers {
 
     constructor(){}
 
     getHeaders(): HttpHeaders {
-        const headers : HttpHeaders = new HttpHeaders();
-        headers.append('Access-Control-Allow-Origin', '*');
-        headers.append('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
-        headers.append("Content-Type" ,  "application/json");
-        headers.append("Access-Control-Allow-Credentials" , "true");
-        headers.append('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Access-Control-Allow-Origin');
-        return headers;
+      let headers : HttpHeaders = new HttpHeaders();
+      headers = headers.append('Authorization', localStorage.getItem(JWTOKEN));
+      headers = headers.append('Access-Control-Allow-Origin', '*');
+      headers = headers.append('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+      headers = headers.append("Content-Type" ,  "application/json");
+      headers = headers.append("Access-Control-Allow-Credentials" , "true");
+      headers = headers.append('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Access-Control-Allow-Origin');
+      return headers;
       }
     
 }
