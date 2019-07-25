2f27e870378494c4ab49ace1d6656ea0ac22be6f
AwaisOfficial <im.awais.official@gmail.com>
2019-07-25 13:40:49 +0500

    twitter login is done.

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,9 +1,10 @@
 "use strict";
+
 Object.defineProperty(exports, "__esModule", { value: true });
 const multer = require('multer');
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './src/uploads');
+        cb(null, '../uploads');
     },
     filename: (req, file, cb) => {
         const filename = new Date().getTime() + '_' + file.originalname;
