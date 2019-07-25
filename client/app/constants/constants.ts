4fff6f247cdb940c1e6b2267c84d2f5f3f15ae29
AwaisOfficial <im.awais.official@gmail.com>
2019-07-25 16:02:41 +0500

    twitter issue fixed.

--- client/app/constants/constants.ts
+++ client/app/constants/constants.ts
@@ -3,3 +3,4 @@ export const JWTOKEN = 'twitter_pro_token';
 export const USER_INFO = 'twitter_pro_user';
 export const EMAIL_PATTERN = '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$';
 export const PASSWORD_PATTERN = '(?=.*[a-z])(?=.*[A-Z])';
+export const ERROR_MSG = 'Something went wrong;';
